e64aff1badcd472e9e1373da909d1016a482309c
AimsLynne <130839244+AimsLynne@users.noreply.github.com>
2023-10-09 18:58:00 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Amy Brown and is{" "}
-        <a
-          href="https://github.com/AimsLynne/project-weather-react"
-          target="blank"
-        >
-          open-sourced
-        </a>{" "}
-        on Github
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Amy Brown and is{" "}
+          <a
+            href="https://github.com/AimsLynne/project-weather-react"
+            target="blank"
+          >
+            open-sourced
+          </a>{" "}
+          on Github
+        </footer>
+      </div>
     </div>
   );
 }
