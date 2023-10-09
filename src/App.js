2869fb7dc71dbda5aefe7bec21339115f2cf4989
AimsLynne <130839244+AimsLynne@users.noreply.github.com>
2023-10-09 17:57:52 +0100

    Added the Github repository URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Amy Brown and is{" "}
+        <a
+          href="https://github.com/AimsLynne/project-weather-react"
+          target="blank"
+        >
+          open-sourced
+        </a>{" "}
+        on Github
+      </footer>
+    </div>
+  );
 }
